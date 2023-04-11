9de2175991386f1dbbd1ca0637ac883ae6c16653
yarn <yswsw90@gamil.com>
2023-04-11 23:51:36 +0900

    ### 로컬 테스트를 위해 타겟수정

--- crawling/src/setupProxy.js
+++ crawling/src/setupProxy.js
@@ -3,7 +3,7 @@ import {createProxyMiddleware}  from "http-proxy-middleware"
 module.exports = (app) => {
     app.use(
         createProxyMiddleware("/api/data", {
-            target: "https://ysw-crawling.fly.dev/api/data",
+            target: "http://localhost",
             changeOrigin: true,
         })
     )
